61b9b0dc96f3e2cb46de3ff0f4c883401a055f6a
A658310 <A658310@A658310-DEN-OEM.local>
2015-07-16 13:33:22 -0600

    early work on task, test failing still

--- lib/task.rb
+++ lib/task.rb
@@ -1,4 +1,19 @@
 class Task
+  attr_accessor :title
+  attr_accessor :priority
+  attr_accessor :completed
+def initalize(title, priority = 10)
+self.title = title
+self.priority = priority
+self.completed = false
+end
 
 
+  def complete
+    @complete = true
+  end
+
+  def complete?
+    @complete
+  end
 end
