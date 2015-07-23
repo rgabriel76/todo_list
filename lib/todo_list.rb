fad5be2d4e84848c4f3eb9cafab94a0190f274ab
A658310 <A658310@A658310-DEN-OEM.local>
2015-07-23 11:13:48 -0600

    does some stuff

--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -1,8 +1,18 @@
 #!/usr/bin/env ruby
 
 class TodoList
+attr_accessor :tasks
 
+def inititialize
+  self.tasks = []
 end
+
+def add(task)
+  self.tasks << task
+end
+
+ 
+
 
 if __FILE__ == $0
 
--- task.rb
+++ task.rb
@@ -2,13 +2,14 @@ class Task
   attr_accessor :title
   attr_accessor :priority
   attr_accessor :completed
+def initalize(title, priority = 10)
+self.title = title
+self.priority = priority
+self.completed = false
+end
 
-  def initialize(title, priority = 10)
-   self.title = title
-   self.priority = priority
-   self.completed = false
-  end
 
+end
   def complete
     @complete = true
   end
